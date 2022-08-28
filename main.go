5102eff3c218fcf92483f6110175201ace534a59
bobg <bobg@emphatic.com>
2022-08-28 13:33:00 -0700

    Add commentary to main.go.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -17,6 +16,8 @@ import (
 
 const namedCardAPIEndpoint = "https://api.scryfall.com/cards/named"
 
+// The function "main" in the package "main"
+// is where a Go program begins execution.
 func main() {
 
 	// Parse command-line flags.
@@ -31,6 +32,7 @@ func main() {
 	// Join remaining command-line words together to make the card name.
 	name := strings.Join(flag.Args(), " ")
 
+	// This constructs a URL for querying the API.
 	u, err := url.Parse(namedCardAPIEndpoint)
 	if err != nil {
 		log.Fatalf("Error parsing named-card API endpoint: %s", err)
@@ -42,45 +44,82 @@ func main() {
 	}
 	u.RawQuery = v.Encode()
 
+	// The creates a new client for performing HTTP requests.
+	// Unlike the default HTTP client in Go,
+	// this one has a custom "Transport" object.
+	// This is the component of an HTTP client
+	// that is responsible for actually conducting the conversation
+	// with the remote server.
+	// Our custom Transport object,
+	// rateLimitedRoundTripper,
+	// makes sure that no two requests to the server
+	// happen less than 100 milliseconds apart
+	// (as requested in the "Good Citizenship" section at
+	// https://scryfall.com/docs/api).
 	cl := http.Client{
 		Transport: rateLimitedRoundTripper{
 			limiter: rate.NewLimiter(rate.Every(100*time.Millisecond), 1),
 		},
 	}
 
-	fmt.Printf("xxx about to get %s\n", u)
-
+	// This performs an HTTP GET request,
+	// using the custom client created above,
+	// to query the API using the URL created further above.
 	resp, err := cl.Get(u.String())
 	if err != nil {
 		log.Fatalf("Error getting %s: %s", u, err)
 	}
 	defer resp.Body.Close()
 
-	dec := json.NewDecoder(resp.Body)
-
-	var obj respObj
+	// The response from the server is in JSON format
+	// ("JavaScript Object Notation," see https://www.json.org).
+	// This creates a JSON decoder that reads from the response body,
+	// then uses the decoder to parse that response into the variable "obj".
+	var (
+		dec = json.NewDecoder(resp.Body)
+		obj respObj
+	)
 	err = dec.Decode(&obj)
 	if err != nil {
 		log.Fatalf("Error decoding response: %s", err)
 	}
 
+	// This creates a JSON encoder.
+	// For now, we're just re-encoding the data we parsed above,
+	// and showing it to the user.
+	// In this case, the encoder writes to "os.Stdout",
+	// which is simply this program's "standard output"
+	// (see https://en.wikipedia.org/wiki/Standard_streams#Standard_output_(stdout)).
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
 	enc.Encode(obj)
 }
 
+// This defines a type to contain the information we parse from the /cards/named endpoint.
+// The actual response has many more data fields than the ones we're pulling out here.
+// The complete description is at https://scryfall.com/docs/api/cards.
 type respObj struct {
 	Name    string    `json:"name"`
 	Prices  pricesObj `json:"prices"`
 	SetName string    `json:"set_name"`
 }
 
+// This defines the type of the "prices" field in a respObj.
 type pricesObj struct {
 	USD       string `json:"usd"`
 	USDFoil   string `json:"usd_foil"`
 	USDEtched string `json:"usd_etched"`
 }
 
+// A rateLimitedRoundTripper is a RoundTripper with a rate limit.
+// Its RoundTrip method calls Wait on the rate limiter
+// to make sure enough time has passed since the last call.
+// After the Wait, it uses Go's default round-tripper
+// to do the actual HTTP conversation.
+//
+// See https://pkg.go.dev/net/http#RoundTripper
+// for a description of the RoundTripper interface
+// that this type implements.
 type rateLimitedRoundTripper struct {
 	limiter *rate.Limiter
 }
